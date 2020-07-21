d9a6a302ff6772208ca96419a0d896530850d29c
Daniel Samus <samus.daniel@gmail.com>
2020-07-21 13:16:52 +1000

    default-location-searches.js

--- src/default-location-searches.js
+++ src/default-location-searches.js
@@ -11,7 +11,7 @@ export default [
   {
     id: 'default-new-york',
     predictionPlace: {
-      address: 'New York City, New York, USA',
+      address: 'University of Sydney',
       bounds: new LatLngBounds(
         new LatLng(40.917576401307, -73.7008392055224),
         new LatLng(40.477399, -74.2590879797556)
@@ -21,7 +21,7 @@ export default [
   {
     id: 'default-los-angeles',
     predictionPlace: {
-      address: 'Los Angeles, California, USA',
+      address: 'University of Technology Sydney',
       bounds: new LatLngBounds(
         new LatLng(34.161440999758, -118.121305008073),
         new LatLng(33.9018913203336, -118.521456965901)
@@ -31,7 +31,7 @@ export default [
   {
     id: 'default-san-francisco',
     predictionPlace: {
-      address: 'San Francisco, California, USA',
+      address: 'University of New South Whales',
       bounds: new LatLngBounds(
         new LatLng(37.8324430069081, -122.354995082683),
         new LatLng(37.6044780500533, -122.517910874663)
@@ -41,7 +41,7 @@ export default [
   {
     id: 'default-seattle',
     predictionPlace: {
-      address: 'Seattle, Washington, USA',
+      address: 'Australian Catholic University',
       bounds: new LatLngBounds(
         new LatLng(47.7779392908564, -122.216605992108),
         new LatLng(47.3403950185547, -122.441233019046)
@@ -51,7 +51,7 @@ export default [
   {
     id: 'default-portland',
     predictionPlace: {
-      address: 'Portland, Oregon, USA',
+      address: 'Macquarie University',
       bounds: new LatLngBounds(
         new LatLng(45.858099013046, -122.441059986416),
         new LatLng(45.3794799927623, -122.929215816001)
